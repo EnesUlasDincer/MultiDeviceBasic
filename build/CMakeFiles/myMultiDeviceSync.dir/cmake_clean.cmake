d6e846a3d8aef97fb39ea1ef0546789b319af94a
EnesUlasDincer <enes.ulas.dincer@gmail.com>
2024-12-26 17:58:29 +0100

    it works.

---
+++ build/CMakeFiles/myMultiDeviceSync.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myMultiDeviceSync.dir/src/MultiDeviceSync.cpp.o"
+  "CMakeFiles/myMultiDeviceSync.dir/src/MultiDeviceSync.cpp.o.d"
+  "CMakeFiles/myMultiDeviceSync.dir/src/cJSON.c.o"
+  "CMakeFiles/myMultiDeviceSync.dir/src/cJSON.c.o.d"
+  "myMultiDeviceSync"
+  "myMultiDeviceSync.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/myMultiDeviceSync.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
